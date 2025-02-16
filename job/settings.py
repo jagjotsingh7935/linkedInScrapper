6812d3e3a1e8f2ca8b3c7e567b313ababf6c84a1
jagjotsingh7935 <jajgotsingh7935@gmail.com>
2025-02-16 20:25:06 +0530

    slight Changes

--- job/settings.py
+++ job/settings.py
@@ -30,7 +30,19 @@ ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_ALL_ORIGINS = True 
+CORS_ALLOWED_ORIGINS = [
+    "https://job-app-ashen.vercel.app",
+    "http://localhost:5173",  # Your Vite development server
+]
 
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 
 
 # Application definition
