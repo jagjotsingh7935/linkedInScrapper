fbe7bc3501ef382d29d5b0bc6617ddceea329a54
jagjotsingh7935 <jajgotsingh7935@gmail.com>
2025-02-22 22:53:23 +0530

    New Changes

--- job/settings.py
+++ job/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
